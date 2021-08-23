bd2b98539f5625432a89e7a4c69bafc4a30a5b29
JuliaNikonovaa <Julia1406@mail.ru>
2021-08-23 21:53:21 +0300

    Point#3

--- sandbox/src/main/java/ru/stqa/ptf/sandbox2/Point.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox2/Point.java
@@ -2,22 +2,13 @@ package ru.stqa.ptf.sandbox2;
 
 public class Point {
 
-	public double x1;
-	public double y1;
-	public double x2;
-	public double y2;
+	public double x;
+	public double y;
 
-	public Point p1(double x1, double y1) {
-		this.x1 = x1;
-		this.y1 = y1;
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
-	public Point p2(double x2, double y2) {
-		this.x2 = x2;
-		this.y2 = y2;
-}
 
-	public static double distance(Point p1, Point p2) {
-		return Math.sqrt((p2.x2-p1.x1)*(p2.x2-p1.x1)+(p2.y2-p1.y1)*(p2.y2-p1.y1));
-
-	}
 }
---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox2/Start.java
@@ -0,0 +1,16 @@
+package ru.stqa.ptf.sandbox2;
+
+public class Start {
+
+	public static void main(String[] args) {
+
+		Point p1 = new Point(4, 5);
+		Point p2 = new Point(6, 8);
+		System.out.println("Distance between p1 and p2 =" + distance(p1, p2));
+	}
+
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+	}
+
+}

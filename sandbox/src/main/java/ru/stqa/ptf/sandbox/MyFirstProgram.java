95d8f34ba3930a65267bf9c54b7020afb50dfb88
JuliaNikonovaa <Julia1406@mail.ru>
2021-08-23 15:01:12 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -1,9 +1,32 @@
 package ru.stqa.ptf.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-	System.out.println("Hello, world!");
-}
+		hello("world");
+		hello("user");
+		hello("Julia");
 
-}
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+		public static void hello(String somebody) {
+
+			System.out.println("Hello, " + somebody + "!");
+
+		}
+
+		public static double area (double len) {
+		return len * len;
+		}
+
+		public static double area(double a, double b) {
+		return a * b;
+		}
+	}

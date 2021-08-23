f149993c08fe063634fa2ee62fd982a6c8faeecb
JuliaNikonovaa <Julia1406@mail.ru>
2021-08-23 19:09:01 +0300

    Point#2

--- sandbox/src/main/java/ru/stqa/ptf/sandbox2/Point.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox2/Point.java
@@ -15,4 +15,9 @@ public class Point {
 		this.x2 = x2;
 		this.y2 = y2;
 }
+
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt((p2.x2-p1.x1)*(p2.x2-p1.x1)+(p2.y2-p1.y1)*(p2.y2-p1.y1));
+
+	}
 }

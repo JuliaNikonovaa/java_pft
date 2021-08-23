4dec60114d5e938c20e6820a50d6f2ef97bc0b43
JuliaNikonovaa <Julia1406@mail.ru>
2021-08-23 18:52:00 +0300

    Point#1

---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox2/Point.java
@@ -0,0 +1,18 @@
+package ru.stqa.ptf.sandbox2;
+
+public class Point {
+
+	public double x1;
+	public double y1;
+	public double x2;
+	public double y2;
+
+	public Point p1(double x1, double y1) {
+		this.x1 = x1;
+		this.y1 = y1;
+	}
+	public Point p2(double x2, double y2) {
+		this.x2 = x2;
+		this.y2 = y2;
+}
+}

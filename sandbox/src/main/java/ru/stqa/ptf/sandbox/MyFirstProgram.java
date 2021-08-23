e955819b18f531c11a914cafcf65468309676bcd
JuliaNikonovaa <Julia1406@mail.ru>
2021-08-23 16:07:29 +0300

    создание методов

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -9,12 +9,11 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Julia");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+				System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+				System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 		public static void hello(String somebody) {
 
@@ -22,11 +21,5 @@ public class MyFirstProgram {
 
 		}
 
-		public static double area (double len) {
-		return len * len;
-		}
 
-		public static double area(double a, double b) {
-		return a * b;
-		}
 	}

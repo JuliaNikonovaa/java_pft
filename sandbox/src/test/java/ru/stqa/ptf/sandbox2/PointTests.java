81d9c0efd0c3de981d864258aea520df21c8c13b
JuliaNikonovaa <Julia1406@mail.ru>
2021-08-26 13:59:42 +0300

    созданы тесты PointTests

---
+++ sandbox/src/test/java/ru/stqa/ptf/sandbox2/PointTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.ptf.sandbox2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+	@Test
+
+
+	public void testdistance() {
+		Point p1 = new Point (4, 5);
+		Point p2 = new Point (6, 8);
+		Assert.assertEquals(p1.distance(p2), 3.605551275463989);
+	}
+@Test
+
+	public void testdistance2() {
+		Point p1 = new Point (4, 5);
+		Point p2 = new Point (6, 8);
+		Assert.assertEquals(p1.distance(p2), 3.0);
+	}
+}

a6a36abfffd34019eb6c21d98f2be0191de17013
JuliaNikonovaa <Julia1406@mail.ru>
2021-08-31 22:10:22 +0300

    тест создание контакта после преобразований

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -1,67 +1,68 @@
 package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ContactCreationTests {
-  private WebDriver wd;
+	private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@BeforeMethod(alwaysRun = true)
+	public void setUp() throws Exception {
+		wd = new FirefoxDriver();
+		wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		wd.get("http://localhost/addressbook/group.php");
+		wd.findElement(By.name("user")).click();
+		wd.findElement(By.name("user")).clear();
+		wd.findElement(By.name("user")).sendKeys("admin");
+		wd.findElement(By.name("pass")).clear();
+		wd.findElement(By.name("pass")).sendKeys("secret");
+		wd.findElement(By.id("LoginForm")).submit();
+		wd.findElement(By.xpath("//div[@id='content']/h1")).click();
+	}
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.id("LoginForm")).submit();
-    wd.findElement(By.xpath("//div[@id='content']/h1")).click();
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Julia");
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Nikonova");
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("89119119191");
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("j.n@kx.com");
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    wd.findElement(By.linkText("home")).click();
-    wd.findElement(By.linkText("Logout")).click();
-  }
+	@Test
+	public void testGroupCreation() throws Exception {
+		wd.findElement(By.linkText("add new")).click();
+		wd.findElement(By.name("firstname")).click();
+		wd.findElement(By.name("firstname")).clear();
+		wd.findElement(By.name("firstname")).sendKeys("Julia");
+		wd.findElement(By.name("lastname")).click();
+		wd.findElement(By.name("lastname")).clear();
+		wd.findElement(By.name("lastname")).sendKeys("Nikonova");
+		wd.findElement(By.name("mobile")).click();
+		wd.findElement(By.name("mobile")).clear();
+		wd.findElement(By.name("mobile")).sendKeys("89119119191");
+		wd.findElement(By.name("email")).click();
+		wd.findElement(By.name("email")).clear();
+		wd.findElement(By.name("email")).sendKeys("j.n@kx.com");
+		wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+		wd.findElement(By.linkText("home")).click();
+		wd.findElement(By.linkText("Logout")).click();
+	}
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
+	@AfterMethod(alwaysRun = true)
+	public void tearDown() throws Exception {
+		wd.quit();
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			wd.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private boolean isAlertPresent() {
+		try {
+			wd.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
 }

00655c45a8150cc3f33b0b0e565910a1196718e5
JuliaNikonovaa <Julia1406@mail.ru>
2021-09-01 23:34:07 +0300

    2.7#1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,17 +6,21 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-	private WebDriver wd;
+	WebDriver wd;
+
+	private NavigationHelper navigationHelper;
+	private GroupHelper groupHelper;
 
 	public void init() {
 		wd = new FirefoxDriver();
 		wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		wd.get("http://localhost/addressbook/group.php?selected%5B%5D=1&selected%5B%5D=2&delete=Delete+group%28s%29");
+		groupHelper = new GroupHelper(wd);
+		navigationHelper = new NavigationHelper(wd);
 		login("admin", "secret");
 	}
 
@@ -35,41 +39,13 @@ public class ApplicationManager {
 		wd.findElement(By.linkText("Logout")).click();
 	}
 
-	public void returnToGroupPage() {
-		wd.findElement(By.linkText("group page")).click();
-	}
-
-	public void submitGroupCreation() {
-		wd.findElement(By.name("submit")).click();
-	}
-
-	public void fillGroupForm(GroupData groupData) {
-		wd.findElement(By.name("group_name")).click();
-		wd.findElement(By.name("group_name")).clear();
-		wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-		wd.findElement(By.name("group_header")).click();
-		wd.findElement(By.name("group_header")).clear();
-		wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-		wd.findElement(By.name("group_footer")).click();
-		wd.findElement(By.name("group_footer")).clear();
-		wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-	}
-
-	public void initGroupCreation() {
-		wd.findElement(By.name("new")).click();
-	}
-
-	public void gotoGroupPage() {
-		wd.findElement(By.linkText("groups")).click();
-	}
-
 	public void stop() {
-		wd.quit();
+		navigationHelper.wd.quit();
 	}
 
 	public boolean isElementPresent(By by) {
 		try {
-			wd.findElement(by);
+			groupHelper.wd.findElement(by);
 			return true;
 		} catch (NoSuchElementException e) {
 			return false;
@@ -78,57 +54,57 @@ public class ApplicationManager {
 
 	public boolean isAlertPresent() {
 		try {
-			wd.switchTo().alert();
+			groupHelper.wd.switchTo().alert();
 			return true;
 		} catch (NoAlertPresentException e) {
 			return false;
 		}
 	}
 
-	public void deleteSelectedGroups() {
-		wd.findElement(By.name("delete")).click();
-	}
-
-	public void selectGroup() {
-		wd.findElement(By.name("selected[]")).click();
-	}
-
 	public void returnToContactPage() {
-		wd.findElement(By.linkText("home")).click();
+		groupHelper.wd.findElement(By.linkText("home")).click();
 	}
 
 	public void enterContact() {
-		wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+		groupHelper.wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
 	}
 
 	public void fillContactForm(ContactData contactData) {
-		wd.findElement(By.name("firstname")).click();
-		wd.findElement(By.name("firstname")).clear();
-		wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
-		wd.findElement(By.name("lastname")).click();
-		wd.findElement(By.name("lastname")).clear();
-		wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-		wd.findElement(By.name("mobile")).click();
-		wd.findElement(By.name("mobile")).clear();
-		wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-		wd.findElement(By.name("email")).click();
-		wd.findElement(By.name("email")).clear();
-		wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+		groupHelper.wd.findElement(By.name("firstname")).click();
+		groupHelper.wd.findElement(By.name("firstname")).clear();
+		groupHelper.wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
+		groupHelper.wd.findElement(By.name("lastname")).click();
+		groupHelper.wd.findElement(By.name("lastname")).clear();
+		groupHelper.wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+		groupHelper.wd.findElement(By.name("mobile")).click();
+		groupHelper.wd.findElement(By.name("mobile")).clear();
+		groupHelper.wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+		groupHelper.wd.findElement(By.name("email")).click();
+		groupHelper.wd.findElement(By.name("email")).clear();
+		groupHelper.wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
 	}
 
 	public void gotoAddNewContactPage() {
-		wd.findElement(By.linkText("add new")).click();
+		groupHelper.wd.findElement(By.linkText("add new")).click();
 	}
 
 	public void closeDialogPage() {
-		wd.switchTo().alert().accept();
+		groupHelper.wd.switchTo().alert().accept();
 	}
 
 	public void deleteSelectedContact() {
-		wd.findElement(By.xpath("//input[@value='Delete']")).click();
+		groupHelper.wd.findElement(By.xpath("//input[@value='Delete']")).click();
 	}
 
 	public void selectContact() {
-		wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td/input")).click();
+		groupHelper.wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td/input")).click();
+	}
+
+	public GroupHelper getGroupHelper() {
+		return groupHelper;
+	}
+
+	public NavigationHelper getNavigationHelper() {
+		return navigationHelper;
 	}
 }

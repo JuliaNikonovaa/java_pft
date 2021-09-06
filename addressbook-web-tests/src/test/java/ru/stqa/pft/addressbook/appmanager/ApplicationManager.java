41ee9f93076854469b3e782b6a872f70266cb8be
JuliaNikonovaa <Julia1406@mail.ru>
2021-09-06 14:17:03 +0300

    запуск теста в трех браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
@@ -24,12 +25,12 @@ public class ApplicationManager {
 	}
 
 	public void init() {
-				if (browser == BrowserType.FIREFOX) {
+				if (browser.equals(BrowserType.FIREFOX)) {
 		wd = new FirefoxDriver();
-	} else if (browser == BrowserType.CHROME) {
+	} else if (browser.equals(BrowserType.CHROME)) {
 			wd = new ChromeDriver();
-		} else if (browser == BrowserType.IE) {
-			wd = new InternetExplorerDriver();
+		} else if (browser.equals(BrowserType.EDGE)) {
+			wd = new EdgeDriver();
 		}
 		wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		wd.get("http://localhost/addressbook/group.php?selected%5B%5D=1&selected%5B%5D=2&delete=Delete+group%28s%29");

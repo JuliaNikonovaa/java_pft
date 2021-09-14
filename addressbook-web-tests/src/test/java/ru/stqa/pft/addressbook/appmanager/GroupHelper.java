03c4fc5da10e37c8c4681dbb3438dad14c35eaef
JuliaNikonovaa <Julia1406@mail.ru>
2021-09-14 22:16:58 +0300

    4.6 сравнение коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -38,8 +38,7 @@ public class GroupHelper extends HelperBase {
 
 	public void selectGroup(int index) {
 		wd. findElements(By.name("selected[]")).get(index).click();
-		click(By.name("selected[]"));
-	}
+			}
 
 	public void initGroupModification() {
 		click(By.name("edit"));

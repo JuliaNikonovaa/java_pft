0aa8c9f07940bd53a15b8205a939978fd6880519
JuliaNikonovaa <Julia1406@mail.ru>
2021-09-14 22:59:55 +0300

    4.7. Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -38,7 +38,7 @@ public class GroupHelper extends HelperBase {
 
 	public void selectGroup(int index) {
 		wd. findElements(By.name("selected[]")).get(index).click();
-			}
+		}
 
 	public void initGroupModification() {
 		click(By.name("edit"));
@@ -68,7 +68,8 @@ public class GroupHelper extends HelperBase {
 		List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
 		for (WebElement element : elements) {
 			String name = element.getText();
-			GroupData group =  new GroupData(name, null, null);
+			String id = element.findElement(By.tagName("input")).getAttribute("value");
+			GroupData group =  new GroupData(id, name, null, null);
 			groups.add(group);
 		}
 		return groups;

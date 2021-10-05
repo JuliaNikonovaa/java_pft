165569aff9689f057af44687caefccb9927e6aef
JuliaNikonovaa <Julia1406@mail.ru>
2021-10-05 20:48:33 +0300

    7.4. Сравнение данных, загруженных из БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -20,6 +20,8 @@ public class ApplicationManager {
 	private SessionHelper sessionHelper;
 	private NavigationHelper navigationHelper;
 	private String browser;
+	private DbHelper dbHelper;
+
 
 	public ApplicationManager(String browser)  {
 		this.browser = browser;
@@ -29,6 +31,8 @@ public class ApplicationManager {
 	public void init() throws IOException {
 		String target = System.getProperty("target", "local");
 		properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+		dbHelper = new DbHelper();
 
 		if (browser.equals(BrowserType.FIREFOX)) {
 			wd = new FirefoxDriver();
@@ -65,5 +69,9 @@ public class ApplicationManager {
 
 	public ContactHelper contact() {
 		return contactHelper;
+	}
+
+	public DbHelper db() {
+		return dbHelper;
 	}
 }
